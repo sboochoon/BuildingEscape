a323e6673b6b42f226e951633db3d0e98a9d320c
Shaad Boochoon <shaadb@gmail.com>
2017-12-03 21:49:40 -0500

    -Finished the room layout -Added OpenDoor component -Finished PositionReporter component

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -0,0 +1,37 @@
+// Shaad Boochoon 2017
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
+#include "GameFramework/Actor.h"
+#include "OpenDoor.generated.h"
+
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	float OpenAngle = 90.f;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate;
+	
+};
